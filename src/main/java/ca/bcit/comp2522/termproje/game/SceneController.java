2d2ca561debb20db8ed79127525e6bcc3d08e08c
Junyoung Lee <tim000421@gmail.com>
2023-03-20 19:03:33 -0700

    Added necessary Scenes

--- src/main/java/ca/bcit/comp2522/termproje/game/SceneController.java
+++ src/main/java/ca/bcit/comp2522/termproje/game/SceneController.java
@@ -34,5 +34,43 @@ public class SceneController {
         stage.show();
     }
 
+    public void switchToEat(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("EatScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    public void switchToSleep(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SleepScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToTraining(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("TrainingScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToMiniGame(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MiniGameScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToRelationship(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("RelationshipScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
