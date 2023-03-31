57adaaac47d6fa59bb6e30f77265f4b982bc9426
Junyoung Lee <tim000421@gmail.com>
2023-03-31 01:51:34 -0700

    Added functions to update stats

--- src/main/java/ca/bcit/comp2522/termproje/game/Player.java
+++ src/main/java/ca/bcit/comp2522/termproje/game/Player.java
@@ -1,6 +1,8 @@
 package ca.bcit.comp2522.termproje.game;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Player class.
  * @authors Patty Hsu & Tim Lee
@@ -12,6 +14,7 @@ public class Player {
     private int strength;
     private int money;
     private int days;
+    private int hunger;
     private ArrayList items;
 
     private String name;
@@ -25,6 +28,7 @@ public class Player {
         speed = 1;
         strength = 1;
         money = 10;
+        hunger = 10;
         days = 1;
         items = new ArrayList<>();
     }
@@ -127,11 +131,112 @@ public class Player {
         this.items = items;
     }
 
+    /**
+     * Gets player name.
+     * @return player name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Sets the player's name
+     * @param name a string
+     */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * Decrease player energy by 5
+     */
+    public void useEnergy() {
+        this.energy -= 5;
+    }
+
+    /**
+     * Decrease player money by 2
+     */
+    public void useMoney() {
+        this.money -= 2;
+    }
+
+    /**
+     * Increase player money by 2
+     */
+    public void earnMoney() {
+        this.money += 2;
+    }
+
+    /**
+     * Update player speed by given amount
+     * @param amount an integer between 1-3
+     */
+    public void updateSpeed(int amount) {
+        this.speed += amount;
+    }
+
+    /**
+     * Update player strength by given amount
+     * @param amount an integer between 1-3
+     */
+    public void updateStrength(int amount) {
+        this.strength += amount;
+    }
+
+    /**
+     * Decrease player hunger by 5.
+     */
+    public void decreaseHunger() {
+        this.hunger -= 5;
+    }
+
+    /**
+     * Increase player hunger by 5.
+     */
+    public void increaseHunger() {
+        this.hunger += 5;
+    }
+
+    public void decreaseHappiness(int amount) {
+        this.happiness -= amount;
+    }
+
+    public void increaseHappiness(int amount) {
+        this.happiness += amount;
+    }
+
+    /**
+     * Update player's speed and strength by 1-3.
+     * Use energy and hunger.
+     * Decrease happiness.
+     */
+    public void trainPlayer() {
+        Random random = new Random();
+        updateSpeed(random.nextInt(3)+1);
+        updateStrength(random.nextInt(3)+1);
+        useEnergy();
+        decreaseHunger();
+    }
+
+    /**
+     * Increase day by 1 and reset energy to 10.
+     */
+    public void sleep() {
+        this.days += 1;
+        setEnergy(10);
+    }
+
+    public void hangout() {
+        Random random = new Random();
+        increaseHappiness(random.nextInt(3)+1);
+        useMoney();
+        useEnergy();
+    }
+    public void eat() {
+        Random random = new Random();
+        increaseHappiness(random.nextInt(3)+1);
+        useMoney();
+        increaseHunger();
     }
 }
