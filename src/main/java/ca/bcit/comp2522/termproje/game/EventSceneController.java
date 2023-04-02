215323c747c6ecb4798aadc683a384c806a850a8
Junyoung Lee <tim000421@gmail.com>
2023-04-02 13:42:30 -0700

    Combined scene controllers into one

--- src/main/java/ca/bcit/comp2522/termproje/game/EventSceneController.java
+++ src/main/java/ca/bcit/comp2522/termproje/game/EventSceneController.java
@@ -1,36 +1,20 @@
 package ca.bcit.comp2522.termproje.game;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
-/**
- * Training scene controller class.
- * @author Patty Hsu & Tim Lee
- * @version 202310
- */
-public class TrainingSceneController {
+
+public class EventSceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    /**
-     * Relationship scene controller class.
-     * @author Patty Hsu & Tim Lee
-     * @version 202310
-     */
+
     public void switchToHome(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HomeScene.fxml")));
-//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("HomeScene.fxml"));
         root = loader.load();
         HomeSceneController homeSceneController = loader.getController();
